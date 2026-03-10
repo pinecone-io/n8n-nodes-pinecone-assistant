1ae67c909c81616e24369da0c30af28f5dc54940
Jenna Pederson <jenna@pinecone.io>
2026-03-10 14:44:10 -0500

    Open link in new tab

--- credentials/PineconeApi.credentials.ts
+++ credentials/PineconeApi.credentials.ts
@@ -38,7 +38,7 @@ export class PineconeApi implements ICredentialType {
             },
             {
                 displayName:
-                        "Start building with Pinecone before May 1, 2026 to receive platform credits when upgrading to Pinecone's Standard plan. Learn more and claim this offer <a href='https://app.pinecone.io/?integration=pinecone-n8n-assistant-node'>here<a/>.",
+                        "Start building with Pinecone before May 1, 2026 to receive platform credits when upgrading to Pinecone's Standard plan. Learn more and claim this offer <a href='https://app.pinecone.io/?integration=pinecone-n8n-assistant-node' target='_blank' rel='noopener noreferrer'>here</a>.",
                 name: 'notice',
                 type: 'notice',
                 default: '',
--- nodes/PineconeAssistant/v1/actions/versionDescription.ts
+++ nodes/PineconeAssistant/v1/actions/versionDescription.ts
@@ -59,7 +59,7 @@ export const versionDescription: INodeTypeDescription = {
         {
             // eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
             displayName:
-                    "Start building with Pinecone before May 1, 2026 to receive platform credits when upgrading to Pinecone's Standard plan. Learn more and claim this offer <a href='https://app.pinecone.io/?integration=pinecone-n8n-assistant-node'>here<a/>.",
+                    "Start building with Pinecone before May 1, 2026 to receive platform credits when upgrading to Pinecone's Standard plan. Learn more and claim this offer <a href='https://app.pinecone.io/?integration=pinecone-n8n-assistant-node' target='_blank' rel='noopener noreferrer'>here</a>.",
             name: 'callout',
             type: 'callout',
             default: '',
